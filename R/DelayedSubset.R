43a8ea735de6624684bc167d8b9c759c5374a716
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-05-01 01:40:18 -0700

    Added documentation for all methods.

--- R/DelayedSubset.R
+++ R/DelayedSubset.R
@@ -1,8 +1,33 @@
+#' Saving a DelayedSubset
+#'
+#' Save a \linkS4class{DelayedSubset} object into a HDF5 file.
+#'
+#' @param x A \linkS4class{DelayedSubset} object.
+#' @param file String containing the path to a HDF5 file.
+#' @param name String containing the name of the group to save into.
+#' 
+#' @return A \code{NULL}, invisibly.
+#' A group is created at \code{name} containing the contents of the DelayedSubset.
+#'
+#' @details
+#' The DelayedSubset class is represented on file by several elements within the \code{name}d group.
+#' \itemize{
+#' \item \code{type}, a character dataset with two elements.
+#' The first is set to \code{"OPERATION"} and the second is set to \code{"SUBSET"}.
+#' \item \code{index}, a group containing \code{number}, the number of dimensions,
+#' and any number of integer datasets with names in [1, number].
+#' Each dataset contains 1-based indices to subset the named dimension of the array.
+#' \item \code{seed}, a group or dataset containing the seed to be subsetted.
+#' }
+#'
+#' @author Aaron Lun
+#'
 #' @examples
 #' X <- DelayedArray(matrix(runif(100), ncol=20))
 #' Y <- X[1:2,3:5]
 #' temp <- tempfile(fileext=".h5")
 #' saveDelayedOps(Y, temp)
+#' rhdf5::h5ls(temp)
 #' 
 #' @export
 #' @importFrom rhdf5 h5createGroup h5write
@@ -10,7 +35,7 @@ setMethod("saveLayer", "DelayedSubset", function(x, file, name) {
     if (name!="") {
         h5createGroup(file, name)
     }
-    h5write(c("operation", "SUBSET"), file, file.path(name, "type"))
+    h5write(c("OPERATION", "SUBSET"), file, file.path(name, "type"))
 
     h5createGroup(file, file.path(name, "index"))
     indices <- x@index
@@ -20,4 +45,5 @@ setMethod("saveLayer", "DelayedSubset", function(x, file, name) {
     }
 
     saveLayer(x@seed, file, file.path(name, "seed"))
+    invisible(NULL)
 })
--- R/DelayedUnaryIsoOpWithArgs.R
+++ R/DelayedUnaryIsoOpWithArgs.R
@@ -1,8 +1,40 @@
+#' Saving a DelayedUnaryIsoOpWithArgs
+#'
+#' Save a \linkS4class{DelayedUnaryIsoOpWithArgs} object into a HDF5 file.
+#'
+#' @param x A \linkS4class{DelayedUnaryIsoOpWithArgs} object.
+#' @param file String containing the path to a HDF5 file.
+#' @param name String containing the name of the group to save into.
+#' 
+#' @return A \code{NULL}, invisibly.
+#' A group is created at \code{name} containing the contents of the DelayedUnaryIsoOpWithArgs.
+#'
+#' @details
+#' The DelayedUnaryIsoOpWithArgs class is represented on file by several elements within the \code{name}d group.
+#' \itemize{
+#' \item \code{type}, a character dataset with two elements.
+#' The first is set to \code{"OPERATION"} and the second is set to \code{"UNARY_WITH_ARGS"}.
+#' \item \code{left_arguments}, a group containing information about the arguments to apply to the seed on the left.
+#' \item \code{left_along}, an integer dataset of length equal to the number of left arguments.
+#' \item \code{right_arguments}, a group containing information about the arguments to apply to the seed on the right.
+#' \item \code{right_along}, an integer dataset of length equal to the number of right arguments.
+#' }
+#'
+#' Each argument in \code{left_arguments} is represented by a 1-dimensional dataset of the relevant type, named according to the positional index of that argument.
+#' This dataset has number of elements equal to the length of the seed dimension referenced in the corresponding element of \code{left_along}.
+#' The idea is to apply the operation to the seed along the referenced dimension, recycling the argument's values across the entire array.
+#' The same logic applies for \code{right_arguments} and \code{right_along}.
+#'
+#' We strongly recommend reading the documentation for \linkS4class{DelayedUnaryIsoOpWithArgs} to understand what's going on here.
+#' 
+#' @author Aaron Lun
+#'
 #' @examples
 #' X <- DelayedArray(matrix(runif(100), ncol=5))
 #' Y <- (1:20 + X) / runif(5)
 #' temp <- tempfile(fileext=".h5")
 #' saveDelayedOps(Y, temp)
+#' rhdf5::h5ls(temp)
 #' 
 #' @export
 #' @importFrom rhdf5 h5createGroup h5write
@@ -37,22 +69,17 @@ setMethod("saveLayer", "DelayedUnaryIsoOpWithArgs", function(x, file, name) {
             along <- x@Ralong
         }
 
-        xpath <- file.path(name, side)
-        h5createGroup(file, xpath)
-
-        argpath <- file.path(xpath, "arguments") 
+        argpath <- file.path(name, paste0(side, "_arguments"))
         h5createGroup(file, argpath)
         for (i in seq_along(args)) {
             h5write(args[[i]], file, file.path(argpath, i))
         }
 
-        alongpath <- file.path(xpath, "along") 
-        h5createGroup(file, alongpath)
-        for (i in seq_along(along)) {
-            h5write(along[[i]], file, file.path(alongpath, i))
-        }
+        alongpath <- file.path(name, paste0(side, "_along"))
+        h5write(along, file, alongpath)
     }
 
     saveLayer(x@seed, file, file.path(name, "seed"))
+    invisible(NULL)
 })
 
