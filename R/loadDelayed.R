14d8321019bf97fdcb60901dc708c10c13dfa346
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-03 14:35:57 -0700

    Enable loading of custom operations/arrays.

--- R/loadDelayed.R
+++ R/loadDelayed.R
@@ -18,6 +18,9 @@
 #' saveDelayed(Z, temp)
 #' loadDelayed(temp)
 #'
+#' @seealso
+#' \code{\link{knownOperations}} and \code{\link{knownArrays}}, to modify the loading procedure.
+#'
 #' @export
 loadDelayed <- function(file, path="delayed") {
     .dispatch_loader(file, path)
@@ -31,35 +34,116 @@ loadDelayed <- function(file, path="delayed") {
         vals <- .load_list(file, path)
 
     } else if (identical(attrs$delayed_type, "operation")) {
-        FUN <- switch(attrs$delayed_operation,
-            subset=.load_delayed_subset,
-            transpose=.load_delayed_aperm,
-            combine=.load_delayed_combine,
-            `binary arithmetic`=.load_delayed_nary_iso,
-            `binary comparison`=.load_delayed_nary_iso,
-            `binary logic`=.load_delayed_nary_iso,
-            dimnames=.load_delayed_dimnames,
-            `subset assignment`=.load_delayed_subassign,
-            `unary arithmetic`=.load_delayed_unary_iso,
-            `unary comparison`=.load_delayed_unary_iso,
-            `unary logic`=.load_delayed_unary_iso,
-            `unary math`=.load_delayed_unary_iso,
-            `unary special check`=.load_delayed_unary_iso
-        )
+        FUN <- known.env$operations[[attrs$delayed_operation]]
+        if (is.null(FUN)) {
+            stop("unknown operation type '", attrs$delayed_operation, "'")
+        }
         vals <- FUN(file, path)
 
     } else if (identical(attrs$delayed_type, "array")) {
-        FUN <- switch(attrs$delayed_array,
-            `dense array`=.load_array,
-            `sparse matrix`=.load_csparse_matrix,
-            `external hdf5 dense array`=.load_dense_hdf5_array,
-            `external hdf5 sparse matrix`=.load_sparse_hdf5_matrix
-        ) 
+        FUN <- known.env$arrays[[attrs$delayed_array]]
+        if (is.null(FUN)) {
+            stop("unknown array type '", attrs$delayed_array, "'")
+        }
         vals <- FUN(file, path)
 
     } else {
-        stop("unsupported type '", attrs$delayed_array[1], "'")
+        stop("unsupported type '", attrs$delayed_type[1], "'")
     }
 
     vals
+}
+
+known.env <- new.env()
+
+known.env$operations <- list(
+    subset=.load_delayed_subset,
+    transpose=.load_delayed_aperm,
+    combine=.load_delayed_combine,
+    `binary arithmetic`=.load_delayed_nary_iso,
+    `binary comparison`=.load_delayed_nary_iso,
+    `binary logic`=.load_delayed_nary_iso,
+    dimnames=.load_delayed_dimnames,
+    `subset assignment`=.load_delayed_subassign,
+    `unary arithmetic`=.load_delayed_unary_iso,
+    `unary comparison`=.load_delayed_unary_iso,
+    `unary logic`=.load_delayed_unary_iso,
+    `unary math`=.load_delayed_unary_iso,
+    `unary special check`=.load_delayed_unary_iso
+)
+
+known.env$arrays <- list(
+    `dense array`=.load_array,
+    `sparse matrix`=.load_csparse_matrix,
+    `external hdf5 dense array`=.load_dense_hdf5_array,
+    `external hdf5 sparse matrix`=.load_sparse_hdf5_matrix
+)
+
+#' Get or set loaders for operations/arrays
+#'
+#' Get or set loading functions for operations or arrays that were saved into the HDF5 file.
+#' This enables third-party packages to modify the \pkg{chihaya} framework for their own purposes.
+#'
+#' @param operations Named list of loading functions for operations.
+#' Each function should accept the same arguments as \code{\link{loadDelayed}} and return a \linkS4class{DelayedArray} of some kind.
+#' Names should match the \code{delayed_operation} string used to save the operation to file.
+#' @param arrays Named list of loading functions for arrays.
+#' Each function should accept the same arguments as \code{\link{loadDelayed}} and return an array of some kind.
+#' Names should match the \code{delayed_array} string used to save the array to file.
+#'
+#' @return 
+#' If \code{operations} is missing, \code{customLoadOperations} will return a list of the current custom operations that have been registered with \pkg{chihaya}.
+#' If \code{operations} is provided, it is used to define the set of custom operations, and the \emph{previous} set of operations is returned.
+#' The same approach is used for \code{arrays} in \code{customLoadArrays}.
+#'
+#' @details
+#' This function can be used to modify the loading procedure for existing operations/arrays or to add new loaders for new operations/arrays.
+#' For custom operations or arrays, we recommend using a \code{"custom "} prefix in the name to ensure that they do not clash with future additions to the \pkg{chihaya} specification.
+#'
+#' @author Aaron Lun
+#' @examples
+#' library(HDF5Array)
+#' X <- rsparsematrix(100, 20, 0.1)
+#' Y <- DelayedArray(X)
+#' Z <- log2(Y + 1)
+#'
+#' temp <- tempfile(fileext=".h5")
+#' saveDelayed(Z, temp)
+#'
+#' # Overriding an existing operation:
+#' ops <- knownOperations()
+#' old_unary <- ops[["unary math"]]
+#' ops[["unary math"]] <- function(file, path) {
+#'     cat("WHEE!\n")
+#'     old_unary(file, path)
+#' }
+#' old <- knownOperations(ops)
+#' 
+#' # Prints our little message:
+#' loadDelayed(temp)
+#'
+#' # Setting it back.
+#' knownOperations(old)
+#' 
+#' @export
+knownOperations <- function(operations) {
+    prev <- known.env$operations
+    if (missing(operations)) {
+        prev
+    } else {
+        known.env$operations <- operations
+        invisible(prev)
+    }
+}
+
+#' @export
+#' @rdname knownOperations
+knownArrays <- function(arrays) {
+    prev <- known.env$arrays
+    if (missing(arrays)) {
+        prev
+    } else {
+        known.env$arrays <- arrays
+        invisible(prev)
+    }
 }
